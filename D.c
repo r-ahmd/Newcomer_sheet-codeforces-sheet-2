29f29b228d677aeb5c22eb4d01464a2c000400bb
Tahmed <115088617+r-ahmd@users.noreply.github.com>
2022-10-28 16:43:06 +0600

    Create D.c

---
+++ D.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+ 
+int main(void) 
+{
+	  int a,a1,a2,a3,a4;
+  while(1)
+  {
+    scanf("%d", &a);
+    scanf("%d", &a1);
+    scanf("%d", &a2);
+    scanf("%d", &a3);
+    scanf("%d", &a4);
+        if(a==1999)
+        {
+          printf("Correct\n");
+          break;
+        }
+        if(a!=1999)
+        {
+          printf("Wrong\n");
+        }
+     if(a1==1999)
+        {
+          printf("Correct\n");
+          break;
+        }
+        if(a1!=1999)
+        {
+          printf("Wrong\n");
+        }
+     if(a2==1999)
+        {
+          printf("Correct\n");
+          break;
+        }
+        if(a2!=1999)
+        {
+          printf("Wrong\n");
+        }
+     if(a3==1999)
+        {
+          printf("Correct\n");
+          break;
+        }
+        if(a3!=1999)
+        {
+          printf("Wrong\n");
+        }
+     if(a4==1999)
+        {
+          printf("Correct\n");
+          break;
+        }
+        if(a4!=1999)
+        {
+          printf("Wrong\n");
+        }
+  }
+	
+ 
+	return 0;
+}
